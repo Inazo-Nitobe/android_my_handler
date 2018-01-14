cd56f635f340a27a5e20032b333e949efdc7b7f4
Takaaki Hirayama <dev.takaaki.hirayama@gmail.com>
2018-01-14 11:09:33 +0900

    update the text repeatedly with sleep when button is tapped. - sleep should not be executed in main thread. - So child thread can't touch UI, use handler to do.

--- app/src/main/java/com/hirayama/takaaki/dev/myhandler/MainActivity.java
+++ app/src/main/java/com/hirayama/takaaki/dev/myhandler/MainActivity.java
@@ -1,13 +1,68 @@
 package com.hirayama.takaaki.dev.myhandler;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView txt = (TextView)findViewById(R.id.textView);
+
+        final Handler h = new Handler() {
+
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                Log.d(TAG, new Integer(msg.what).toString());
+                txt.setText(new Integer(msg.what).toString());
+            }
+        };
+
+
+        Button b = (Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d(TAG, "countup: start");
+
+                Thread t = new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        for (int i = 0; i < 50; i++) {
+                            try {
+                                Thread.sleep(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            h.sendEmptyMessage(i);
+                            //txt.setText(new Integer(i).toString());
+                        }
+                    }
+                });
+
+                t.start();
+
+            }
+        });
+
     }
+
 }
+
